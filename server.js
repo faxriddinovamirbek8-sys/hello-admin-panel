003b27631dece14cf903aca70b1839dc875a8a7c
Amirbek <faxriddinovamirbek8@gmail.com>
2026-06-18 11:36:28 +0500

    fix railway port

--- server.js
+++ server.js
@@ -2,15 +2,14 @@
 const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server ishlayapti: ${PORT}`);
+  console.log("Server ishlayapti: " + PORT);
 });
